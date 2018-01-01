1d5f369814d7d68123bb5d7f1419ab975ddc0fe8
Pavel Keyzik <pavel.keyzik@gmail.com>
2018-01-02 00:36:40 +0300

    Change port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = new http.Server();
 
-server.listen(1337, 'localhost');
+server.listen(proccess.env.PORT || 3030, 'localhost');
 
 server.on('request', function(req, res) {
     res.end('Hello World!');
