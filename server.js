2908518851463846a994675752a74ea8ea448577
Pavel Keyzik <pavel.keyzik@gmail.com>
2018-01-02 12:50:26 +0300

    Updated code

--- server.js
+++ server.js
@@ -4,11 +4,7 @@ var app = express();
 var port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-<<<<<<< HEAD
   res.send('Hello world! Check it out!');
-=======
-  res.send('Hello world!');
->>>>>>> 5cdd20c51993a996c3408e262b99da53abf62adc
 });
 
 app.listen(port, () => {
