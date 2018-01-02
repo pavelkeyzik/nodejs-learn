56dbecfbb6aaf353cb1a58c7560340290a87defb
Pavel Keyzik <pavel.keyzik@gmail.com>
2018-01-02 16:52:48 +0300

    added .html files

--- server.js
+++ server.js
@@ -1,25 +1,38 @@
 var express = require('express');
-var url = require('url');
+var fs = require('fs');
+var path = require('path');
 var log = require('./log')(module);
 
 var app = express();
+var staticDir = __dirname + '/public';
 
 var port = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
-  res.send('Home Page 1');
-  console.log(req.method, req.url);
+
+  fs.readFile('public/index.html', function (err, info) {
+      if(err) {
+        log.error(err);
+        res.sendStatus(500);
+        res.send('Server error!');
+        return;
+      }
+      res.sendFile(path.join(staticDir, 'index.html'));
+  });
+
 });
 
 app.get('/admin', function (req, res) {
-  var urlParsed = url.parse(req.url, true);
 
-  log.info(urlParsed.query.message);
-  log.debug(urlParsed.query.message);
-
-  if(urlParsed.query.message == undefined) log.error('params not found');
-
-  res.send('Admin page!!');
+  fs.readFile('public/admin.html', function (err, data) {
+     if(err) {
+       log.error(err);
+       res.statusCode(500);
+       res.send('Sever error');
+       return;
+     }
+     res.sendFile(path.join(staticDir, 'admin.html'));
+  });
 
 });
 
